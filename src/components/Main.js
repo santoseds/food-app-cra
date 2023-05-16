5858bb6104706bf4aa5a5e2bda94ffb0ba024849
rhavymaia <rhavy.guedes@ifpb.edu.br>
2023-05-16 15:17:12 -0300

    Listagem de comidas via endpoint.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,41 +1,65 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button, Form, Container, Row } from 'react-bootstrap';
 
-import foods from '../models/foods';
+//import foods from '../models/foods';
 import Food from './Food';
 
 const Main = () => {
   let [nome, setNome] = useState('');
+  let [foods, setFoods] = useState([]);
 
-  const handleClick = (event) => {
-    console.log('Clicou!');
-  };
+  let buttonAdd = useRef(null);
+
+  const handleClick = (event) => {};
 
   const handleChange = (event) => {
-    console.log('Digitou alguma coisa!');
+    setNome(event.target.value);
   };
 
+  useEffect(() => {
+    fetch('http://localhost:4000/comidas', { method: 'GET' })
+      .then((response) => response.json())
+      .then((data) => setFoods([...data]))
+      .catch((error) => {});
+  }, []);
+
   return (
-    <Container>
-      <main>
+    <main>
+      <Container>
         <h1>Menu</h1>
+        <div className="text-right">
+          <Button
+            variant="secondary"
+            className="rounded-circle mr-4 font-weight-bold"
+            onClick={handleClick}
+            ref={buttonAdd}
+          >
+            +
+          </Button>
+        </div>
+
         {/* Component Button do bootstrap. */}
         <Form.Group className="mb-3">
           <Form.Label>Alimento</Form.Label>
-          <Form.Control type="text" placeholder="Café" />
-
-          <input type="text" onChange={handleChange} />
+          <Form.Control
+            type="text"
+            placeholder="Café"
+            onChange={handleChange}
+            value={nome}
+          />
         </Form.Group>
+
         <Button onClick={handleClick} variant="primary">
           Pesquisar
         </Button>
+
         <Row className="my-2">
           {foods.map((food) => (
-            <Food food={food}></Food>
+            <Food key={food.id} food={food}></Food>
           ))}
         </Row>
-      </main>
-    </Container>
+      </Container>
+    </main>
   );
 };
 
