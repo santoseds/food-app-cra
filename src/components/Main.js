3925c47f810fee822058a79e746b77a8785f9d43
rhavymaia <rhavy.guedes@ifpb.edu.br>
2023-06-06 15:17:11 -0300

    Modularização do componente FoodForm.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Button, Form, Container, Row } from 'react-bootstrap';
-import Modal from 'react-bootstrap/Modal';
 
 //import foods from '../models/foods';
 import Food from './Food';
+import FoodForm from './FoodForm';
 
 const Main = () => {
   let [foods, setFoods] = useState([]);
-  let [food, setFood] = useState({ name: '', image: '' });
 
   let [nome, setNome] = useState('');
 
@@ -45,36 +44,8 @@ const Main = () => {
       .catch();
   }, []);
 
-  const handleChange = (event) => {
-    setFood({ ...food, [event.target.name]: event.target.value });
-  };
-
   const nomeHandleChange = (event) => {
     setNome(event.target.value);
-  };
-
-  const handleOnSubmit = (event) => {
-    event.preventDefault();
-    // Enviar os dados para o servidor backend.
-    fetch('http://localhost:4000/foods', {
-      method: 'POST', // Método de envio.
-      body: JSON.stringify(food), // Converte o Json em string
-      headers: {
-        'Content-Type': 'application/json', // Especifica o tipo do conteúdo da requisição.
-      },
-    })
-      .then((response) => {
-        if (response.ok == true) {
-          // Fechar modal.
-          setShow(false);
-          return response.json();
-        }
-      })
-      .then((data) => {
-        setFoods([...foods, data]);
-      })
-      .catch((error) => {});
-    // Atualizar a lista dos itens do cardápio.
   };
 
   return (
@@ -84,11 +55,11 @@ const Main = () => {
         <div className="text-right">
           <Button
             variant="secondary"
-            className="rounded-circle mr-4 font-weight-bold"
+            className="mr-4 font-weight-bold"
             onClick={handleShow}
             ref={buttonAdd}
           >
-            +
+            + | Adicionar Preparação
           </Button>
         </div>
 
@@ -113,56 +84,12 @@ const Main = () => {
           ))}
         </Row>
 
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Cadastro de Comida</Modal.Title>
-          </Modal.Header>
-          <Form onSubmit={handleOnSubmit}>
-            <Modal.Body>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Nome</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Nome"
-                  name="name"
-                  onChange={handleChange}
-                  value={food.name}
-                />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Imagem</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Imagem"
-                  name="image"
-                  onChange={handleChange}
-                  value={food.image}
-                />
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Descrição</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  type="text"
-                  placeholder="Descrição"
-                  name="description"
-                  onChange={handleChange}
-                  value={food.description}
-                />
-              </Form.Group>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Fechar
-              </Button>
-              <Button type="submit" variant="primary">
-                Salvar
-              </Button>
-            </Modal.Footer>
-          </Form>
-        </Modal>
+        <FoodForm
+          show={show}
+          handleClose={handleClose}
+          foods={foods}
+          setFoods={setFoods}
+        ></FoodForm>
       </Container>
     </main>
   );
